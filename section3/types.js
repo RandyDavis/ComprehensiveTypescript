82893dbc22d5aa57a5e94281e21d77a54e09f902
Randy Davis <randy_davis@outlook.com>
2015-07-30 22:59:02 -0500

    create section 3 directory; add types.ts file to show basic types

---
+++ section3/types.js
@@ -0,0 +1,17 @@
+// Common TS Basic Types
+// Number
+var n;
+n = 6;
+//n = "jump!"; error
+// String
+n = 4 + 6 * 3;
+n = Math.PI;
+var s;
+s = "Jim";
+s = "Jim" + "Smith";
+// Boolean
+var isItTrue;
+isItTrue = true;
+isItTrue = false;
+isItTrue = undefined;
+//# sourceMappingURL=types.js.map
---
+++ section3/types.ts
@@ -0,0 +1,25 @@
+// Common TS Basic Types
+
+// Number
+var n: number;
+
+n = 6;
+//n = "jump!"; error
+
+
+// String
+n = 4 + 6 * 3;
+
+n = Math.PI;
+
+var s: string;
+s = "Jim";
+s = "Jim" + "Smith";
+
+
+// Boolean
+var isItTrue: boolean;
+
+isItTrue = true;
+isItTrue = false;
+isItTrue = undefined;
